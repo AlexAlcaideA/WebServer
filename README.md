4692c65343f997a8fdac212df4432a37a59f51a4
Eloy Antoni Pascual Perez-Casaux <epascual@car9s2.42barcelona.com>
2026-08-15 17:54:26 +0200

    Avances y GET request.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
 # WebServer
 
+Usamos ipv4 y poll
+
 ## Resources
 
 - [SNMP]()
@@ -11,22 +13,23 @@
 - [poll](https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/poll.h.html)
 - [CGI](https://en.wikipedia.org/wiki/Common_Gateway_Interface)
 - [HTTP](https://www.rfc-editor.org/info/rfc9111/)
+- [HTTP versions](https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Evolution_of_HTTP)
 - [IETF](https://es.wikipedia.org/wiki/Grupo_de_Trabajo_de_Ingenier%C3%ADa_de_Internet)
 - [Error 418](https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/418)
-- poll -> <poll.h> 
-- select -> <sys/select.h>
-- kqueue
-- epoll -> <sys/epoll.h>
+- [Requests](https://developer.salesforce.com/docs/atlas.en-us.apexref.meta/apexref/apex_classes_restful_http_httprequest.htm)
+- [CGI tests](https://www6.uniovi.es/~antonio/ncsa_httpd/cgi/examples.html)
 
-HTTP requests:
+- HTTP requests:
+**metodos obligatorios**
 GET
 POST
 DELETE
+**metodos opcionales**
 PUT
 OPTIONS
 HEAD
 
-HTTP returns:
+- HTTP returns:
 200 OK: Success consigui enviar el archivo
 201 Created: Se ha creado un nuevo recurso
 204 No Content: Success pero archivo vacio
@@ -42,9 +45,65 @@ HTTP returns:
 500 Internal Server Error: El servidor ha petado o ha encontrado un error
 503 Service Unavailable: El servicio esta temporalmente no disponible
 
-Puertos:
+- Puertos:
 Los puertos son 16 bit unsigned int
+puerto 8080: prueba
 puerto 21:FTP (transferencia de archivos)
 puerto 22:ssh (conexion remota)
 puerto 111:rpcbind o portmapper (sirve para llamadas y network file sistems)
 puerto 631:ipp (sirve para imprimir)
+
+### Funciones
+- poll -> <poll.h> 
+- select -> <sys/select.h>
+- kqueue
+- epoll -> <sys/epoll.h>
+
+
+-[ ]execve
+-[ ]pipe
+-[ ]strerror
+-[ ]gai_strerror
+-[ ]errno
+-[ ]dup
+-[ ]dup2
+-[ ]fork
+-[ ]socketpair
+-[ ]htons
+-[ ]htonl
+-[ ]ntohs
+-[ ]ntohl
+-[ ]select
+-[ ]poll
+-[ ]epoll (epoll_create
+-[ ]epoll_ctl
+-[ ]epoll_wait)
+-[ ]kqueue (kqueue
+-[ ]kevent)
+-[ ]socket
+-[ ]accept
+-[ ]listen
+-[ ]send
+-[ ]recv
+-[ ]shutdown
+-[ ]chdir
+-[ ]bind
+-[ ]connect
+-[ ]getaddrinfo
+-[ ]freeaddrinfo
+-[ ]setsockopt
+-[ ]getsockname
+-[ ]getprotobyname
+-[ ]fcntl
+-[ ]close
+-[ ]read
+-[ ]write
+-[ ]waitpid
+-[ ]kill
+-[ ]signal
+-[ ]access
+-[ ]stat
+-[ ]open
+-[ ]opendir
+-[ ]readdir
+-[]closedir
---
+++ getserv.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main()
+{
+    // 1. Crear socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    // 2. Configurar dirección
+    sockaddr_in address;
+    std::memset(&address, 0, sizeof(address));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    // 3. Bind
+    if (bind(server_fd,
+             (sockaddr *)&address,
+             sizeof(address)) == -1)
+    {
+        perror("bind");
+        close(server_fd);
+        return 1;
+    }
+
+    // 4. Listen
+    if (listen(server_fd, 10) == -1)
+    {
+        perror("listen");
+        close(server_fd);
+        return 1;
+    }
+
+    std::cout << "Server listening on http://localhost:8080\n";
+
+    while (true)
+    {
+        // 5. Aceptar cliente
+        int client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd == -1)
+        {
+            perror("accept");
+            continue;
+        }
+
+        // 6. Leer request HTTP
+        char buffer[4096];
+        std::memset(buffer, 0, sizeof(buffer));
+
+        int bytes = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (bytes <= 0)
+        {
+            close(client_fd);
+            continue;
+        }
+
+        std::string request(buffer);
+
+        std::cout << "----- REQUEST -----\n";
+        std::cout << request;
+        std::cout << "-------------------\n";
+		std::string path;
+
+        // 7. Comprobar que sea GET /
+		if (request.find("GET ") == 0)
+		{
+		    size_t start = 4;
+		    size_t end = request.find(' ', start);
+
+		    path = request.substr(start, end - start);
+
+		    if (path == "/")
+		        path = "/index.html";
+
+		    std::string filename = "./docs" + path;
+			// 8. Abrir archivo
+		    std::ifstream file(filename.c_str());
+
+            if (!file)
+            {
+                std::string response =
+                    "HTTP/1.1 404 Not Found\r\n"
+                    "Content-Length: 13\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Connection: close\r\n"
+                    "\r\n"
+                    "404 Not Found";
+
+                write(client_fd, response.c_str(), response.size());
+            }
+            else
+            {
+                // 9. Leer archivo entero
+                std::stringstream content;
+                content << file.rdbuf();
+
+                std::string body = content.str();
+
+                // 10. Crear respuesta HTTP
+                std::stringstream response;
+
+                response << "HTTP/1.1 200 OK\r\n";
+                response << "Content-Type: text/html\r\n";
+                response << "Content-Length: " << body.size() << "\r\n";
+                response << "Connection: close\r\n";
+                response << "\r\n";
+                response << body;
+
+                std::string response_str = response.str();
+
+                // 11. Enviar respuesta
+                write(client_fd,
+                      response_str.c_str(),
+                      response_str.size());
+            }
+        }
+        else
+        {
+            std::string response =
+                "HTTP/1.1 404 Not Found\r\n"
+                "Content-Length: 13\r\n"
+                "Content-Type: text/plain\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+                "404 Not Found";
+
+            write(client_fd, response.c_str(), response.size());
+        }
+
+        // 12. Cerrar conexión
+        close(client_fd);
+    }
+
+    close(server_fd);
+    return 0;
+}
